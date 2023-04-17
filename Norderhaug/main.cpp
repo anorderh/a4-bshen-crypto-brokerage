30f3e2043ef61d3c5289c3cac72bdf6a91b65908
Anthony NorderhendqCDcd <anthony@norderhaug.org>
2023-04-17 12:43:49 -0700

    summary output

--- Norderhaug/main.cpp
+++ Norderhaug/main.cpp
@@ -23,22 +23,34 @@ int main(int argc, char **argv) {
 
     // 3. Create service objs, data ptrs, & associated threads.
     pthread_t btc_producer; pthread_t eth_producer; // Producers
-    pthread_t blockchain_x_consumer; pthread_t blockchain_y_consumer; // Consumers
+    pthread_t consumer_x; pthread_t consumer_y; // Consumers
 
-    ProducerData* btc_data = new ProducerData(TradeRequestService(Bitcoin),
-                                              &buffer, &synch);
-    ProducerData* eth_data = new ProducerData(TradeRequestService(Ethereum),
-                                              &buffer, &synch);
-    ConsumerData* blockchain_x_data = new ConsumerData(RequestTransactionService(BlockchainX),
-                                                       &buffer, &synch);
-    ConsumerData* blockchain_y_data = new ConsumerData(RequestTransactionService(BlockchainY),
-                                                       &buffer, &synch);
+    auto btc_service = TradeRequestService(Bitcoin);
+    auto eth_service = TradeRequestService(Ethereum);
+    auto blockchain_x = RequestTransactionService(BlockchainX);
+    auto blockchain_y = RequestTransactionService(BlockchainY);
 
+    ProducerData* btc_data = new ProducerData(btc_service,&buffer, &synch);
+    ProducerData* eth_data = new ProducerData(eth_service,&buffer, &synch);
+    ConsumerData* consumer_x_data = new ConsumerData(blockchain_x,&buffer, &synch);
+    ConsumerData* consumer_y_data = new ConsumerData(blockchain_y,&buffer, &synch);
+
+    // For more deterministic output: BTC -> ETH-> BLOCKCHAIN_X -> BLOCKCHAIN_Y
     pthread_create(&btc_producer, nullptr, produce, (void *) btc_data);
     pthread_create(&eth_producer, nullptr, produce, (void *) eth_data);
-    pthread_create(&blockchain_x_consumer, nullptr, consume, (void *) blockchain_x_data);
-    pthread_create(&blockchain_y_consumer, nullptr, consume, (void *) blockchain_y_data);
+    pthread_create(&consumer_x, nullptr, consume, (void *) consumer_x_data);
+    pthread_create(&consumer_y, nullptr, consume, (void *) consumer_y_data);
 
-    // 4. Barrier - Blocking until 1 consumer thread completes. Then finishes, leaving OS to end child threads.
+    // 4. Barrier - Blocking until 1 consumer thread completes, i.e. all produced requests consumed
     sem_wait(&synch.barrier);
+
+    // Ending child threads
+    pthread_cancel(btc_producer);
+    pthread_cancel(eth_producer);
+    pthread_cancel(consumer_x);
+    pthread_cancel(consumer_y);
+
+    // Log requests produced & consumption per blockchains
+    unsigned int* consumer_res[] = {blockchain_x.reqs_consumed, blockchain_y.reqs_consumed};
+    log_production_history(buffer.reqs_produced, consumer_res);
 }
